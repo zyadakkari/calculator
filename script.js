e32530a99e18f9bd90896ea9db904a47ec611d7d
Zyad <>
2022-08-17 14:46:37 +0100

    Updated script to calculate correctly

--- script.js
+++ script.js
@@ -1,43 +1,48 @@
 function showNumber(number){
-    document.querySelector('.calcscreen').innerHTML += number;
+    document.querySelector('.calcscreen').textContent += number;
 }
-let num1;
-let operator;
+let number1 ;
+let operator ;
 
 function playOperator(type){
-    const num1 = document.querySelector('.calcscreen').innerHTML;
-    const operator = type;
-    document.querySelector('.calcscreen').innerHTML = "";
+    number1 = document.querySelector('.calcscreen').textContent;
+    operator = type;
+    document.querySelector('.calcscreen').textContent = "";
 }
 
 function add(num1, num2){
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2){
-    return num1 - num2;
-}
+    return Number(num1) - Number(num2)}
 
 function multiply(num1, num2){
-    return num1 * num2;
-}
+    return Number(num1) * Number(num2)}
 
 function divide(num1, num2){
-    return num1 / num2
+    return Number(num1) / Number(num2)}
+
+const equals = document.querySelector('#equals');
+equals.addEventListener("click", createNum2)
+
+function createNum2(){
+    number2 = document.querySelector('.calcscreen').textContent;
+    let result = operate(operator, number1, number2)
+    document.querySelector('.calcscreen').textContent = result
+    console.log(result)
 }
 
 function operate(operator, num1, num2){
-    console.log(num2, num1, operator)
-    let sign = operator;
-    let result;
-    if (sign === "+"){
-        result = add(num1, num2)
-    } else if (sign === "-"){
-        result = subtract(num1, num2);
-    } else if (sign === "*"){
-        result = multiply(num1, num2);
-    } else if (sign === "/"){
-        result = divide(num1, num2);
+    if (operator === "+"){
+        console.log(num1, num2)
+        console.log(operator)
+        return add(num1, num2)
+    } else if (operator === "-"){
+        return subtract(num1, num2);
+    } else if (operator === "*"){
+        return multiply(num1, num2);
+    } else if (operator === "/"){
+        return divide(num1, num2);
     }
-    console.log(result)
 }
