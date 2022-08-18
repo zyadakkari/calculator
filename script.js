45cc02f79ed5b8b0cceee27b50a43c9461621826
Zyad <>
2022-08-18 09:09:41 +0100

    Updated to allow consecutive operations

--- script.js
+++ script.js
@@ -1,36 +1,66 @@
-function showNumber(number){
-    document.querySelector('.calcscreen').textContent += number;
-}
-let number1 ;
-let operator ;
+let number1 = null ;
+let number2 = null;
+let operator = null ;
+let opClicked = false;
 
-function playOperator(type){
-    number1 = document.querySelector('.calcscreen').textContent;
-    operator = type;
+// gets number input
+function clickNumber(number){
+    // if last click was operator starts number collection again
+    if (opClicked === true){
+        document.querySelector('.calcscreen').textContent = "";
+        // tells computer last click no longer operator
+        opClicked = false;
+        // adds input to calc display
+        document.querySelector('.calcscreen').textContent += number;
+    } else { document.querySelector('.calcscreen').textContent += number;
+    }
+}
+// gets operator input
+function clickOperator(type){
+    if (number1 === null){
+        number1 = document.querySelector('.calcscreen').textContent;
+        operator = type;
+        opClicked = true
+    } else {
+        calculate();
+        result = operate(operator, number1, number2);
+        document.querySelector('.calcscreen').textContent = result;
+        opClicked = true;
+        operator = type;
+
+    }
+
+
+
+//     if (opClicked != true){
+//         opClicked = true;
+//     }
+}
+
+
+const clear = document.querySelector('#clear');
+clear.addEventListener("click", clearAll)
+
+function clearAll(){
     document.querySelector('.calcscreen').textContent = "";
+    number1 = null;
+    number2 = null;
+    operator = null;
+    opClicked = false;
+
 }
-
-function add(num1, num2){
-    return Number(num1) + Number(num2);
-}
-
-function subtract(num1, num2){
-    return Number(num1) - Number(num2)}
-
-function multiply(num1, num2){
-    return Number(num1) * Number(num2)}
-
-function divide(num1, num2){
-    return Number(num1) / Number(num2)}
 
 const equals = document.querySelector('#equals');
-equals.addEventListener("click", createNum2)
+equals.addEventListener("click", calculate)
 
-function createNum2(){
+function calculate(){
     number2 = document.querySelector('.calcscreen').textContent;
-    let result = operate(operator, number1, number2)
+    let result = operate(operator, number1, number2);
+    number1 = result;
+    number2 = null;
     document.querySelector('.calcscreen').textContent = result
     console.log(result)
+    opClicked = false;
 }
 
 function operate(operator, num1, num2){
@@ -46,3 +76,17 @@ function operate(operator, num1, num2){
         return divide(num1, num2);
     }
 }
+
+
+function add(num1, num2){
+    return Number(num1) + Number(num2);
+}
+
+function subtract(num1, num2){
+    return Number(num1) - Number(num2)}
+
+function multiply(num1, num2){
+    return Number(num1) * Number(num2)}
+
+function divide(num1, num2){
+    return Number(num1) / Number(num2)}
