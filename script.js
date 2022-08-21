4fee02e951c4e5c81c851b1305365086113ffddf
Zyad <>
2022-08-21 13:58:54 +0100

    Fixed decimal & divide zero bugs, added backspace

--- script.js
+++ script.js
@@ -2,32 +2,66 @@ let number1 = null ;
 let number2 = null;
 let operator = null ;
 let opClicked = false;
+const display = document.querySelector('.calcscreen');
 
-// gets number input
+
+// gets number input, checking if operator already called
 function clickNumber(number){
-    // if last click was operator starts number collection again
-    if (opClicked === true){
-        document.querySelector('.calcscreen').textContent = "";
-        // tells computer last click no longer operator
-        opClicked = false;
-        // adds input to calc display
-        document.querySelector('.calcscreen').textContent += number;
-    } else { document.querySelector('.calcscreen').textContent += number;
+    if (number != "."){
+        if (opClicked === true){
+            display.textContent = "";
+                // tells computer last click no longer operator
+            opClicked = false;
+                // adds input to calc display
+            display.textContent += number;
+        } else { display.textContent += number;
+        }
+    // if decimal clicked checks number doesnt already contain decimal
+    } else {
+        if (display.textContent.includes(".") && opClicked === true){
+            display.textContent = "";
+            opClicked = false;
+            display.textContent += number;
+        } else if (display.textContent.includes(".") && opClicked === false){
+            console.log("Error: maximum one decimal per number")
+        } else {
+            if (opClicked === true){
+                display.textContent = "";
+                    // tells computer last click no longer operator
+                opClicked = false;
+                    // adds input to calc display
+                display.textContent += number;
+            } else { display.textContent += number;
+            }
+        }
+    }
+}
+
+// backspace button
+const backspace = document.querySelector('#backspace');
+backspace.addEventListener("click", delChar)
+
+function delChar(){
+    if (display.textContent.length > 0){
+        display.textContent = display.textContent.slice(0,-1)
+
     }
 }
 // gets operator input
 function clickOperator(type){
     // checks if consecutive operation or first
     if (number1 === null){
-        number1 = document.querySelector('.calcscreen').textContent;
+        number1 = display.textContent;
         operator = type;
         opClicked = true
     } else {
         calculate();
-        result = operate(operator, number1, number2);
-        document.querySelector('.calcscreen').textContent = result;
-        opClicked = true;
         operator = type;
+        opClicked = true;
+        number1 = display.textContent
+
+
+
     }
 
 }
@@ -37,7 +71,7 @@ const clear = document.querySelector('#clear');
 clear.addEventListener("click", clearAll)
 
 function clearAll(){
-    document.querySelector('.calcscreen').textContent = "";
+    display.textContent = "";
     number1 = null;
     number2 = null;
     operator = null;
@@ -53,11 +87,12 @@ function calculate(){
     if (number1 === null){
         return;
     } else {
-        number2 = document.querySelector('.calcscreen').textContent;
+        number2 = display.textContent;
         let result = operate(operator, number1, number2);
-        number1 = result;
+        number1 = null;
         number2 = null;
-        document.querySelector('.calcscreen').textContent = result
+        operator = null;
+        display.textContent = result
         console.log(result)
         opClicked = false;
     }
